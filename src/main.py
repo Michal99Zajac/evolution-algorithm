40641bfa3806679750c77d36fc80b8e1ee9daeea
Michal Zajac <michal.zajac@tango.agency>
2022-10-28 18:02:19 +0200

    feat(Chromosome): add generate method

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from models.Chromosome import Chromosome
 if __name__ == "__main__":
     result = schaffer_N4(0, 1.25313)
     chromosome_lenght = Chromosome.chromosome_lenght(6, -10, 10)
-    chromosome = Chromosome(chromosome_lenght)
+    chromosome = Chromosome.generate(chromosome_lenght)
     print(chromosome.gens)
-    chromosome.inverse(23, 25)
+    chromosome.inverse(22, 25)
     print(chromosome.gens)
--- src/models/Chromosome.py
+++ src/models/Chromosome.py
@@ -1,12 +1,14 @@
+from __future__ import annotations
 from math import ceil, log2
+from msilib.schema import Error
 import random
 from typing import List
 
 class Chromosome:
     _gens: List[bool] = []
 
-    def __init__(self, length: int):
-        self.__generate(length)
+    def __init__(self, gens: List[bool]):
+        self._gens = gens
 
     def __len__(self):
         return len(self._gens)
@@ -15,8 +17,14 @@ class Chromosome:
         return ''.join(map(lambda gen: '1' if gen else '0', self._gens))
 
     def inverse(self, left: int, right: int):
+        if (left < 0 or right > len(self._gens)):
+            raise Exception("Exception: left or right is out of range")
+
         inversed_subgens = reversed(self._gens[left:right])
         self._gens[left:right] = inversed_subgens
+
+    def cross(self, partner: Chromosome, position: int):
+        pass
 
     @property
     def gens(self):
@@ -26,8 +34,10 @@ class Chromosome:
     def gens(self, new_gens: List[bool]):
         self.gens = new_gens
 
-    def __generate(self, lenght: int):
-        self._gens = [bool(random.getrandbits(1)) for _ in range(lenght)]
+    @staticmethod
+    def generate(lenght: int):
+        gens = [bool(random.getrandbits(1)) for _ in range(lenght)]
+        return Chromosome(gens)
 
     @staticmethod
     def chromosome_lenght(precision: int, left_border: float, right_border: float):
