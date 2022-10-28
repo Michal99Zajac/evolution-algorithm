5f8bf9bba637f3857ee8d65ec28240dfb350f960
Michal Zajac <michal.zajac@tango.agency>
2022-10-28 19:08:12 +0200

    feat(app): add Crossing class

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from fitness import schaffer_N4
 from models.Chromosome import Chromosome
+from models.Crossing import HomogeneousCrossing
 
 if __name__ == "__main__":
     result = schaffer_N4(0, 1.25313)
     chromosome_lenght = Chromosome.chromosome_lenght(6, -10, 10)
-    chromosome_a = Chromosome.generate(chromosome_lenght)
-    chromosome_b = Chromosome.generate(chromosome_lenght)
-    new_chromosome = chromosome_a.cross(chromosome_b, 3)
+    chromosome_a = Chromosome.generate(4)
+    chromosome_b = Chromosome.generate(4)
+    crossing = HomogeneousCrossing()
+    offspring1, offspring2 = crossing.cross(chromosome_a, chromosome_b)
 
     print(chromosome_a.gens)
     print(chromosome_b.gens)
-    print(new_chromosome.gens)
+    print(offspring1.gens)
+    print(offspring2.gens)
--- src/models/Chromosome.py
+++ src/models/Chromosome.py
@@ -22,17 +22,6 @@ class Chromosome:
         inversed_subgens = reversed(self._gens[left:right])
         self._gens[left:right] = inversed_subgens
 
-    def cross(self, partner: Chromosome, position: int):
-        if position < 0 or position > len(self._gens):
-            raise Exception("Error: position is out of range")
-
-        if len(partner) != len(self._gens):
-            raise Exception("Error: chromosomes lengths are not equal")
-
-        my_gens = self._gens[0:position]
-        partner_gens = partner.gens[position:len(self._gens)]
-        return Chromosome(my_gens + partner_gens)
-
     @property
     def gens(self):
         return self._gens
