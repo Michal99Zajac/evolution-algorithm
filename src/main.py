eb7d2355e41cc5a464211bbddcbcd74fc901e7f8
Michal Zajac <michal.zajac@tango.agency>
2022-10-29 14:06:43 +0200

    feat(Mutation): add probability check decorator

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from models.Chromosome import Chromosome
-import processes.crossing as cross
+import processes.crossover as crossover
+from processes.mutation import EdgeMutation
 
 if __name__ == "__main__":
     chromosome_lenght = Chromosome.chromosome_lenght(6, -10, 10)
     chromosome_a = Chromosome.generate(chromosome_lenght)
     chromosome_b = Chromosome.generate(chromosome_lenght)
-    offspring1, offspring2 = cross.one_point(chromosome_a, chromosome_b)
+    mutation = EdgeMutation(0.3)
 
     print(chromosome_a.gens)
-    print(chromosome_b.gens)
-    print(offspring1.gens)
-    print(offspring2.gens)
+    mutation.mutate(chromosome_a)
+    print(chromosome_a.gens)
---
+++ src/processes/mutation.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+import random
+from abc import ABC, abstractmethod
+from typing import Callable
+from typing import TypeVar
+
+TMutation = TypeVar("TMutation", bound="Mutation")
+
+from models.Chromosome import Chromosome
+
+class Mutation(ABC):
+    def __init__(self, probability: float):
+        if probability > 1: raise Exception('Error: probability can\'t be bigger then 100%')
+        self.__probability = probability
+
+    def checker(function: Callable[[TMutation, Chromosome], Chromosome]):
+        def mutate(self: TMutation, chromosome: Chromosome):
+            if self.__probability >= random.random():
+                return function(self, chromosome)
+            return chromosome
+
+        return mutate
+
+    @checker
+    @abstractmethod
+    def mutate(self, chromosome: Chromosome):
+        pass
+
+class EdgeMutation(Mutation):
+    @Mutation.checker
+    def mutate(self, chromosome: Chromosome):
+        chromosome.mutate(len(chromosome) - 1)
